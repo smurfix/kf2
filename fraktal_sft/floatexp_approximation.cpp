smurfix/kf2

commit 6bfeb445f523232f43df1979ec8963b2bcd73ff5

fix crasher bug relating to series approximation memory allocation

fraktal_sft/floatexp_approximation.cpp
@@ -83,8 +83,6 @@ void CFraktalSFT::CalculateApproximation(int nType)
 	//		mindiff = 0.0000001;
 	if (GetNoApprox())
 		mindiff = 0;
-	m_APr[0] = 1;
-	m_APi[0] = 0;
 	if (GetAutoApproxTerms()){
 		int nT = sqrt((double)m_nTotal)*0.021;
 		if (nT<5)
@@ -94,6 +92,8 @@ void CFraktalSFT::CalculateApproximation(int nType)
 		SetApproxTerms(nT);
 	}
 	int m_nTerms = GetApproxTerms();
+	m_APr[0] = 1;
+	m_APi[0] = 0;
 	for (i = 1; i<m_nTerms; i++){
 		m_APr[i] = 0;
 		m_APi[i] = 0;

fraktal_sft/main.cpp
@@ -2486,6 +2486,7 @@ static long OpenSettings(HWND hWnd, bool &ret)
 					if (hWnd)
 						UpdateWindowSize(hWnd);
 					g_SFT.SetImageSize(g_SFT.GetImageWidth(), g_SFT.GetImageHeight());
+					g_SFT.SetApproxTerms(g_SFT.GetApproxTerms());
 					if (hWnd)
 						PostMessage(hWnd,WM_KEYDOWN,VK_F5,0);
 					ret = false;

commit 5aca68ee98ff5c5908b54e03bda305b666479f6c

fix performance regression introduced by 6bfeb445f523232f43df1979ec8963b2bcd73ff5

fraktal_sft/floatexp_approximation.cpp
@@ -92,9 +92,7 @@ void CFraktalSFT::CalculateApproximation(int nType)
 		SetApproxTerms(nT);
 	}
 	int m_nTerms = GetApproxTerms();
-	m_APr[0] = 1;
-	m_APi[0] = 0;
-	for (i = 1; i<m_nTerms; i++){
+	for (i = 0; i<m_nTerms; i++){
 		m_APr[i] = 0;
 		m_APi[i] = 0;
 	}