bd21896a045c987efe4677aee068b0fdd90cc67e
Matthias Urlichs <matthias@urlichs.de>
2021-12-28 21:00:00 +0100

    Nonexistent: CreateLists()

--- fraktal_sft/fraktal_sft.h
+++ fraktal_sft/fraktal_sft.h
@@ -717,7 +717,6 @@ public:
 // XXX dead/unused
     //BOOL(LongDoubleAlways)
     //BOOL(FloatExpAlways)
-	void CreateLists();
 	int nPos; // possibly used for debugging
 	std::string GetPosition();  // not defined
 
