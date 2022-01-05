b6091a486af46c9d2336f4c021b7fa0cbf519db4
Matthias Urlichs <matthias@urlichs.de>
2022-01-05 15:44:02 +0100

    don't build the debug version of embedded lib by default

--- embed.mk
+++ embed.mk
@@ -3,7 +3,8 @@ WINPREFIX ?= /usr
 SIMD ?= 4
 OPENCL ?= 1
 COMPILE ?= g++ -march=native
-TYPEFLAGS ?= -march=native -fPIC -I ./embed/ -g -Og
+TYPEFLAGS ?= -march=native -fPIC -I ./embed/
+# -g -Og
 LINK ?= g++ -g
 LIBS ?= -lHalf -lmpfr -lgsl -lIlmImf -lglfw -lclew -lOpenCL -ljpeg -ltiff -lpng -lpixman-1 -ldl -lpthread
 WINDRES ?= false
